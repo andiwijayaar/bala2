669460ac876cfb898e30cd70e6a1a8e77b2932f8
Andi Wijaya <andi.wijaya@aprisma.co.id>
2016-11-14 09:57:25 +0700

    Update mike

---
+++ TeamSuccess/MenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  MenuViewController.h
+//  TeamSuccess
+//
+//  Created by Andi Wijaya on 11/11/16.
+//  Copyright © 2016 MC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MenuViewController : UITableViewController
+
+@end
